45a6742ac7c6578b8aad2e7feb8c5a903efc6b36
kyu <kyu@Ctrip.com>
2015-04-15 18:22:11 +0800

    add BIListCell

---
+++ Bike/Bike/View/ListView/BIListCell.h
@@ -0,0 +1,16 @@
+//
+//  BIHomeCell.h
+//  Bike
+//
+//  Created by kyu on 15/4/13.
+//  Copyright (c) 2015年 kyu. All rights reserved.
+//
+
+#import "BIBaseTableViewCell.h"
+#import "coupons.h"
+#import "UIImageView+WebCache.h"
+
+@interface BIListCell : BIBaseTableViewCell
+
+
+@end
---
+++ Bike/Bike/View/ListView/BIListCell.m
@@ -0,0 +1,63 @@
+//
+//  BIHomeCell.m
+//  Bike
+//
+//  Created by kyu on 15/4/13.
+//  Copyright (c) 2015年 kyu. All rights reserved.
+//
+
+#import "BIListCell.h"
+#import "BIAPIService.h"
+
+@interface BIListCell(){
+    UILabel *lbl;
+    UIImageView *img;
+}
+
+@end
+
+@implementation BIListCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+- (void)updateCell:(id)model{
+    id superObj = [super initWithFrame:self.frame separateMode:UISeparatableViewSeparateModeNone];
+    BILog(@"%@",superObj);
+    [self createCellContentView];
+    
+    coupons *md = (coupons *)model;
+    lbl.text = md.cid;
+    lbl.font = [UIFont systemFontOfSize:15.0];
+    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"%@%@",[BIAPIService getClientAppAPIBaseURLString],md.cpic]];
+    [img sd_setImageWithPreviousCachedImageWithURL:url andPlaceholderImage:[UIImage imageNamed:@"overlay.png"] options:SDWebImageDelayPlaceholder progress:^(NSInteger receivedSize, NSInteger expectedSize) {
+        BILog(@"%ld",receivedSize/expectedSize);
+    } completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
+        BILog(@"loadSuccess %@",imageURL);
+    }];
+    
+    [self setSeparateMode:UISeparatableViewSeparateModeBottom];
+}
+
+- (void)createCellContentView{
+    if(!lbl){
+        lbl = [[UILabel alloc]init];
+        [self addSubview:lbl];
+    }
+    if(!img){
+        img = [[UIImageView alloc]init];
+        [self addSubview:img];
+    }
+    
+    img.frame = CGRectMake(0, 0, self.viewHeight, self.viewHeight);
+    lbl.frame = CGRectMake(img.viewXRight+15, 10, self.viewWidth-30-img.viewWidth, 15);
+}
+
+@end
