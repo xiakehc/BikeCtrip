90fd7442b426c809ff526feece731f18902e8651
kyu <kyu@Ctrip.com>
2015-04-16 10:59:23 +0800

    add masonry

--- Bike/Pods/Target Support Files/Pods/Pods-environment.h
+++ Bike/Pods/Target Support Files/Pods/Pods-environment.h
@@ -66,6 +66,12 @@
 #define COCOAPODS_VERSION_MINOR_HJRSeparatableView 1
 #define COCOAPODS_VERSION_PATCH_HJRSeparatableView 0
 
+// Masonry
+#define COCOAPODS_POD_AVAILABLE_Masonry
+#define COCOAPODS_VERSION_MAJOR_Masonry 0
+#define COCOAPODS_VERSION_MINOR_Masonry 6
+#define COCOAPODS_VERSION_PATCH_Masonry 1
+
 // SDWebImage
 #define COCOAPODS_POD_AVAILABLE_SDWebImage
 #define COCOAPODS_VERSION_MAJOR_SDWebImage 3
